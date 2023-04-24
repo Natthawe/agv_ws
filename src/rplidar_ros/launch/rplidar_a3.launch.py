e608396b57353e43535b316a5e4c396adae6997f
Natthawe <natthawejumjai@gmail.com>
2023-04-24 11:32:03 +0700

    update

--- src/rplidar_ros/launch/rplidar_a3.launch.py
+++ src/rplidar_ros/launch/rplidar_a3.launch.py
@@ -1,7 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     return LaunchDescription([
         Node(
@@ -10,9 +9,9 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'serial_port': '/dev/ttyUSB1',
                 'serial_baudrate': 256000,  # A3
-                'frame_id': 'laser_link',
+                'frame_id': 'laser',
                 'inverted': False,
                 'angle_compensate': False,
                 'scan_mode': 'Sensitivity',
