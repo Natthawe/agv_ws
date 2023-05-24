7afaf829ae71977c4513750b12458779fdda5c94
Natthawe <natthawejumjai@gmail.com>
2023-05-24 10:13:38 +0700

    update

--- src/rplidar_ros/launch/rplidar_a3.launch.py
+++ src/rplidar_ros/launch/rplidar_a3.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'serial_port': '/dev/CP2102',
                 'serial_baudrate': 256000,  # A3
                 'frame_id': 'laser',
                 'inverted': False,
