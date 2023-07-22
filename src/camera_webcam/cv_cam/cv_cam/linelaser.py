15b78087c6d3bf2766cb5b81846f29ec47f699e2
Natthawe <natthawejumjai@gmail.com>
2023-07-22 15:19:57 +0700

    fixed bug

--- src/camera_webcam/cv_cam/cv_cam/linelaser.py
+++ src/camera_webcam/cv_cam/cv_cam/linelaser.py
@@ -71,6 +71,11 @@ class CombinedDetectionNode(Node):
 
         # Variable to track the last time an obstacle was detected
         self.last_obstacle_time = time.time()
+        
+        self.obstacle_detected = False
+        
+        self.stop_time = 0.0
+        self.last_line_detection_time = time.time()        
 
         # Line detection variables
         self.RLine = 0
@@ -91,42 +96,35 @@ class CombinedDetectionNode(Node):
     def obstacle_detection_callback(self, msg):
         # Convert LaserScan angles to radians
         angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
-                
+
         # Filter the laser scan data within the desired range
         filtered_ranges = []
-        filtered_angles = []
         for angle, distance in zip(angles, msg.ranges):
             if self.min_angle <= np.degrees(angle) <= self.max_angle:
                 filtered_ranges.append(distance)
-                filtered_angles.append(angle)
 
-        # # Print the filtered distance and angle data
-        # for angle, distance in zip(filtered_angles, filtered_ranges):
-        #     self.get_logger().info(f"Angle: {np.degrees(angle):.2f} degrees, Distance: {distance:.2f} meters")
+        # Check for obstacles within the desired range
+        self.obstacle_detected = any(distance < 0.65 for distance in filtered_ranges)  # Adjust the threshold as needed
 
-                # Check for obstacles within the desired range
-        obstacle_detected = any(distance < 1.0 for distance in filtered_ranges)  # Adjust the threshold as needed
+        if self.obstacle_detected:
+            # Stop the robot immediately when an obstacle is detected
+            # self.publish_velocity(0.0, 0.0)
 
-        # Stop or move forward based on obstacle detection
-        if obstacle_detected:
-            self.twist_cmd.linear.x = 0.0  # Set linear speed to 0 to stop
-            self.last_obstacle_time = time.time()  # Update the time when an obstacle was last detected
+            self.stop_time = time.time()
+            self.obstacle_detected = True
+            self.publish_velocity(0.0, 0.0)
         else:
-            # Check if it's time to move forward again after the delay
-            if time.time() - self.last_obstacle_time >= 2.0:
-                self.twist_cmd.linear.x = 0.2  # Set the linear speed to move forward
-
-        # Publish the Twist command
-        self.publisher.publish(self.twist_cmd) 
-
-        # Check if the obstacle is cleared, then resume line following
-        if not obstacle_detected and self.twist_cmd.linear.x > 0:
-            # Line following logic
-            angular_speed = self.calculate_angular_speed(self.middle)
-            linear_speed = self.calculate_linear_speed(angular_speed)
-            self.publish_velocity(linear_speed, angular_speed)        
+            # If no obstacle is detected, check the time difference before allowing the robot to move forward again
+            if self.obstacle_detected and time.time() - self.stop_time >= 2.0:
+                self.obstacle_detected = False
 
     def line_detection_callback(self, msg):
+        # Check if there is a delay needed after obstacle detection
+        if self.obstacle_detected and time.time() - self.stop_time < 2.0:
+            # Delay the line detection callback for 2 seconds after stopping
+            time.sleep(2.0 - (time.time() - self.stop_time))
+            return       
+        
         # Convert the ROS2 Image message to OpenCV format
         bridge = CvBridge()
         frame = bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -145,99 +143,108 @@ class CombinedDetectionNode(Node):
         num_rows = min(5, frame.shape[0] // 15)
 
         for i in range(num_rows):
-            start_height.append(thresh.shape[0] - 1 - (15 * i))
-            signed_thresh = thresh[start_height[i]].astype(np.int16)
-            diff.append(np.diff(signed_thresh))
+                start_height.append(thresh.shape[0] - 1 - (15 * i))
+                signed_thresh = thresh[start_height[i]].astype(np.int16)
+                diff.append(np.diff(signed_thresh))
 
-            points.append(np.where(np.logical_or(diff[i] > 200, diff[i] < -200)))
-            cv2.line(frame, (0, start_height[i]), (thresh.shape[1], start_height[i]), (255, 0, 0), 2)
+                points.append(np.where(np.logical_or(diff[i] > 200, diff[i] < -200)))
+                cv2.line(frame, (0, start_height[i]), (thresh.shape[1], start_height[i]), (255, 0, 0), 2)
 
-            self.point_count = 0
-            for j in range(len(points[i][0])):
-                self.middle = points[i][0][j]
-                cv2.circle(frame, (self.middle, start_height[i]), 5, (0, 0, 255), 2)
-                cv2.circle(frame, (thresh.shape[1] // 2, start_height[i]), 5, (255, 0, 0), 2)
-                cv2.line(frame, (self.middle, start_height[i]), (thresh.shape[1] // 2, start_height[i]), (0, 255, 0), 2)
-                
-                
-                if i == 4:
-                    self.point_count += 1
-                
-                
-        if self.point_count != self.last_point_count:
-            self.last_point_count = self.point_count
-            print("point_count:", self.point_count)
-
-            if self.point_count >= 10:
-                if self.mark == 0:
-                    self.count += 1
-                    self.mark = 1
-                    print("Count:", self.count)
-                    print("RLine:", self.RLine)
-            
-            if self.point_count == 2:
-                self.mark = 0
-                if self.count >= 2:
-                    if self.RLine == 1:
-                        self.RLine = 0
-                    else:
-                        self.RLine = 1
-                    self.count = 0
-                    print("Count:", self.count)
-                    print("RLine:", self.RLine)
-
-        if len(points) >= 5:
-            if self.RLine == 1:
-                if len(points[4][0]) > 0:
-                    rr = len(points[4][0])
-                    self.middle = points[4][0][rr-1] 
-                    #print("R5",self.middle)
-            else :
-                if len(points[4][0]) > 0:
-                    self.middle = points[4][0][0]
-                    #print("L5",self.middle)
+                self.point_count = 0
+                for j in range(len(points[i][0])):
+                    self.middle = points[i][0][j]
+                    cv2.circle(frame, (self.middle, start_height[i]), 5, (0, 0, 255), 2)
+                    cv2.circle(frame, (thresh.shape[1] // 2, start_height[i]), 5, (255, 0, 0), 2)
+                    cv2.line(frame, (self.middle, start_height[i]), (thresh.shape[1] // 2, start_height[i]), (0, 255, 0), 2)
                     
-            if self.middle == 0:
-                if self.RLine == 1:
-                    if len(points[3][0]) > 0:
-                        rr = len(points[3][0])
-                        self.middle = points[3][0][rr-1] 
-                        #print("R5",self.middle)
-                else :
-                    if len(points[3][0]) > 0:
-                        self.middle = points[3][0][0]
-                        #print("L5",self.middle)
-            if self.middle == 0:
-                if self.RLine == 1:
-                    if len(points[2][0]) > 0:
-                        rr = len(points[2][0])
-                        self.middle = points[2][0][rr-1] 
-                        #print("R5",self.middle)
-                else :
-                    if len(points[2][0]) > 0:
-                        self.middle = points[2][0][0]
-                        #print("L5",self.middle)
-            if self.middle == 0:
-                if self.RLine == 1:
-                    if len(points[1][0]) > 0:
-                        rr = len(points[1][0])
-                        self.middle = points[1][0][rr-1] 
-                        #print("R5",self.middle)
-                else :
-                    if len(points[1][0]) > 0:
-                        self.middle = points[1][0][0]
-                        #print("L5",self.middle)
-                
-        if self.middle == 0:
-            self.get_logger().info("Not found!!!")
-            self.publish_velocity(0.0, 0.0)
-        else:   
-            angular_speed = self.calculate_angular_speed(self.middle)
-            linear_speed = self.calculate_linear_speed(angular_speed)
-            self.publish_velocity(linear_speed, angular_speed)
+                    
+                    if i == 4:
+                        self.point_count += 1
+                    
+                    
+                if self.point_count != self.last_point_count:
+                    self.last_point_count = self.point_count
+                    print("point_count:", self.point_count)
+
+                    if self.point_count >= 10:
+                        if self.mark == 0:
+                            self.count += 1
+                            self.mark = 1
+                            print("Count:", self.count)
+                            print("RLine:", self.RLine)
+                    
+                    if self.point_count == 2:
+                        self.mark = 0
+                        if self.count >= 2:
+                            if self.RLine == 1:
+                                self.RLine = 0
+                            else:
+                                self.RLine = 1
+                            self.count = 0
+                            print("Count:", self.count)
+                            print("RLine:", self.RLine)
+
+                if len(points) >= 5:
+                    if self.RLine == 1:
+                        if len(points[4][0]) > 0:
+                            rr = len(points[4][0])
+                            self.middle = points[4][0][rr-1] 
+                            #print("R5",self.middle)
+                    else :
+                        if len(points[4][0]) > 0:
+                            self.middle = points[4][0][0]
+                            #print("L5",self.middle)
+                            
+                    if self.middle == 0:
+                        if self.RLine == 1:
+                            if len(points[3][0]) > 0:
+                                rr = len(points[3][0])
+                                self.middle = points[3][0][rr-1] 
+                                #print("R5",self.middle)
+                        else :
+                            if len(points[3][0]) > 0:
+                                self.middle = points[3][0][0]
+                                #print("L5",self.middle)
+                    if self.middle == 0:
+                        if self.RLine == 1:
+                            if len(points[2][0]) > 0:
+                                rr = len(points[2][0])
+                                self.middle = points[2][0][rr-1] 
+                                #print("R5",self.middle)
+                        else :
+                            if len(points[2][0]) > 0:
+                                self.middle = points[2][0][0]
+                                #print("L5",self.middle)
+                    if self.middle == 0:
+                        if self.RLine == 1:
+                            if len(points[1][0]) > 0:
+                                rr = len(points[1][0])
+                                self.middle = points[1][0][rr-1] 
+                                #print("R5",self.middle)
+                        else :
+                            if len(points[1][0]) > 0:
+                                self.middle = points[1][0][0]
+                                #print("L5",self.middle)
         
+        if self.obstacle_detected:
+            # If an obstacle is detected while on the line, stop the robot
+            angular_speed = 0.0
+            linear_speed = 0.0
+        else:
+            # Line following logic
+            if self.middle != 0:
+                angular_speed = self.calculate_angular_speed(self.middle)
+                linear_speed = self.calculate_linear_speed(angular_speed)
+            else:
+                # If the line is lost, stop the robot
+                angular_speed = 0.0
+                linear_speed = 0.0
+                self.get_logger().info("Not found!!!")
+
+        self.publish_velocity(linear_speed, angular_speed)
+
         img_msg = bridge.cv2_to_imgmsg(frame, "bgr8")
-        self.image_pub.publish(img_msg)        
+        self.image_pub.publish(img_msg)           
 
     def calculate_linear_speed(self, angular_speed):
         if self.previous_angular_speed != 0.0 and angular_speed != self.previous_angular_speed:
@@ -256,11 +263,18 @@ class CombinedDetectionNode(Node):
         val = err / 800
         return val
 
+    # def publish_velocity(self, linear, angular):
+    #     twist_msg = Twist()
+    #     twist_msg.linear.x = linear
+    #     twist_msg.angular.z = angular
+    #     self.publisher.publish(twist_msg)
+        
     def publish_velocity(self, linear, angular):
         twist_msg = Twist()
         twist_msg.linear.x = linear
         twist_msg.angular.z = angular
         self.publisher.publish(twist_msg)
+        return linear  # Return the calculated linear value        
 
 def main(args=None):
     rclpy.init(args=args)
